1f60f00b13dabd7cd00b0c8fecd7406b35ca1d2c
Sean McPherson <smcpherson@niche.com>
2020-12-07 21:47:12 -0500

    feat: add search-params.js

---
+++ search-params.js
@@ -0,0 +1,30 @@
+/**
+ * Retrieve the params from window.location
+ * @typedef {Object} Config
+ * @property {string} param
+ * @property {string} name
+ * @property {(value: string) => string} mapping
+ * @param {...(Config|string)} keys
+ * @returns {Object}
+ */
+export function get() {
+  const usp = new URLSearchParams(location.search);
+  const output = {};
+
+  for (let arg of arguments) {
+    let key, value;
+    if (typeof arg === "string") {
+      key = arg;
+      value = usp.getAll(arg);
+    } else {
+      let params = usp.getAll(arg.param);
+
+      key = arg.name ? arg.name : arg.param;
+      value = arg.mapping ? params.map(arg.mapping) : params;
+    }
+
+    output[key] = value.length === 1 ? value[0] : value;
+  }
+
+  return output;
+}
